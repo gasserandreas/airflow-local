a391c05406d79ee13259804011a4a6265bea9098
Andreas Liistro <gasserandreas@users.noreply.github.com>
2025-02-13 20:33:02 +0100

    next iteration

--- dags/process_employee.py
+++ dags/process_employee.py
@@ -2,12 +2,10 @@ import datetime
 import pendulum
 import os
 
-# from airflow.models.dag import DAG
-
+import requests
 from airflow.decorators import dag, task
-
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
-# from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 @dag(
     dag_id="process_employees",
@@ -19,15 +17,29 @@ from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 def ProcessEmployees():
   create_employees_table = SQLExecuteQueryOperator(
     task_id="create_employees_table",
-    # postgres_conn_id="postgres_default",
-    conn_id="local_postgres",
+    conn_id="airflow_data",
     sql="""
-    CREATE TABLE IF NOT EXISTS employees (
-      employee_id SERIAL PRIMARY KEY,
-      first_name VARCHAR NOT NULL,
-      last_name VARCHAR NOT NULL,
-      hire_date DATE NOT NULL,
-      termination_date DATE
+      CREATE TABLE IF NOT EXISTS employees (
+        "Serial Number" NUMERIC PRIMARY KEY,
+        "Company Name" TEXT,
+        "Employee Markme" TEXT,
+        "Description" TEXT,
+        "Leave" INTEGER
+      );
+    """,
+  )
+
+  create_employees_temp_table = SQLExecuteQueryOperator(
+    task_id="create_employees_temp_table",
+    conn_id="airflow_data",
+    sql="""
+      DROP TABLE IF EXISTS employees_temp;
+      CREATE TABLE employees_temp (
+          "Serial Number" NUMERIC PRIMARY KEY,
+          "Company Name" TEXT,
+          "Employee Markme" TEXT,
+          "Description" TEXT,
+          "Leave" INTEGER
     );
     """,
   )
@@ -35,8 +47,58 @@ def ProcessEmployees():
   @task
   def echo_something():
     print("something")
+
+  @task
+  def get_data():
+    data_path = "/opt/airflow/data/employees.csv"
+    os.makedirs(os.path.dirname(data_path), exist_ok=True)
+
+    url = "https://raw.githubusercontent.com/apache/airflow/main/docs/apache-airflow/tutorial/pipeline_example.csv"
+
+    response = requests.request("GET", url)
+
+    with open(data_path, "w") as file:
+      file.write(response.text)
+
+    postgres_hook = PostgresHook(postgres_conn_id="airflow_data")
+    conn = postgres_hook.get_conn()
+    cur = conn.cursor()
+    with open(data_path, "r") as file:
+      cur.copy_expert(
+        "COPY employees_temp FROM STDIN WITH CSV HEADER DELIMITER AS ',' QUOTE '\"'",
+        file,
+      )
+    
+    conn.commit()
+
+  @task
+  def merge_data():
+    query = """
+      INSERT INTO employees
+      SELECT *
+      FROM (
+        SELECT DISTINCT *
+        FROM employees_temp
+      ) t
+      ON CONFLICT ("Serial Number") DO UPDATE
+      SET
+        "Employee Markme" = excluded."Employee Markme",
+        "Description" = excluded."Description",
+        "Leave" = excluded."Leave";
+    """
+
+    try:
+      postgres_hook = PostgresHook(postgres_conn_id="airflow_data")
+      conn = postgres_hook.get_conn()
+      cur = conn.cursor()
+      cur.execute(query)
+      conn.commit()
+      return 0
+    except Exception as e:
+      print(e)
+      return 1
   
-  create_employees_table >> echo_something()
+  [create_employees_table, create_employees_temp_table] >> echo_something() >> get_data() >> merge_data()
 
 
 dag = ProcessEmployees()
